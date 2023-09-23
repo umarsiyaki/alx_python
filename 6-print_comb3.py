a31e65634b2f78e851353e6f65149a0a40ec6f7c
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-23 11:37:06 +0100

    git successful

--- 6-print_comb3.py
+++
@@ -1,4 +0,0 @@
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f'{i}{j:02d}', end=', ')
-print()  # To add a newline after the last combination
---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    for j in range(i + 1, 10):
+        print('{}{}, '.format(i, j), end='')
+print()  # To add a newline after the last combination
