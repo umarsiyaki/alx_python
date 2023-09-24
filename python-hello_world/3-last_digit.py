09f6007c4381b2a89f9867fa9620fcf023f9ed81
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-24 21:30:11 +0100

    printed negative value

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,8 +8,10 @@ last_digit = abs(number) % 10
 # Determine its relationship with 5 and 0
 if last_digit > 5:
     print(f' Last digit of {number} is {last_digit} and is greater than 5')
+    
+    else:
+        print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
+        
 elif last_digit == 0:
     print(f' Last digit of {number} is {last_digit} and is 0')
-else:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
     
