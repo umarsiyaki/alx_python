0b2797cf6ca6344c078a4007431d86ef56623f8f
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-24 21:23:53 +0100

    last digit printed

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,9 +7,9 @@ last_digit = abs(number) % 10
 
 # Determine its relationship with 5 and 0
 if last_digit > 5:
-    print(f'The string Last digit of {number} is {last_digit} and is greater than 5')
+    print(f' Last digit of {number} is {last_digit} and is greater than 5')
 elif last_digit == 0:
-    print(f'The string Last digit of {number} is {last_digit} and is 0')
+    print(f' Last digit of {number} is {last_digit} and is 0')
 else:
-    print(f'The string Last digit of {number} is {last_digit} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
     
