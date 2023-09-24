fcf502d460db45eda682691aef36bb338ab135b6
umarsiyaki@gmail.com <umarsiyaki@gmail.com>
2023-09-24 21:32:16 +0100

    else entered

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 if number > 0:
     print('98 is positive')
-elif number == 0:
+else number == 0:
     print('0 is zero')
-else:
+elif:
     print('-98 is negative')
